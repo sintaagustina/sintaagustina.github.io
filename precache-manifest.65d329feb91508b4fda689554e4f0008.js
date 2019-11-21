7d79222d92d2f075c938b13cc1434b43d72f03aa
sintaagustina <yasintaagustinas@gmail.com>
2019-11-21 13:55:00 +0700

    UGD PWA

---
+++ precache-manifest.65d329feb91508b4fda689554e4f0008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60662ebf66c4d5e53ea",
+    "url": "/css/chunk-vendors.9aee28a3.css"
+  },
+  {
+    "revision": "68e319a072ae1a16248dc926e0fc139f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a275bef7c4b1ffff9e1",
+    "url": "/js/app.67a7b6f9.js"
+  },
+  {
+    "revision": "f60662ebf66c4d5e53ea",
+    "url": "/js/chunk-vendors.13e6486c.js"
+  },
+  {
+    "revision": "510e98a71a4fbc354092e82bc6df720a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
